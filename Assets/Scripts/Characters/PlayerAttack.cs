44ba3611b9a12db76f4ef362e8ad677ef0465e74
EndocrineMSc <bamsc.studio@gmail.com>
2023-04-29 22:10:33 +0200

    Aiming is now pretty good

--- Assets/Scripts/Characters/PlayerAttack.cs
+++ Assets/Scripts/Characters/PlayerAttack.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
-using UnityEngine.UIElements;
-using TMPro;
 
 namespace Characters
 {
@@ -18,10 +13,14 @@ namespace Characters
         //Aiming
         private GameObject _aimRecticle;
         private float _aimSpeed = 1.2f;
-        private bool _isAiming;
-        private Ease _aimEase = Ease.Linear;
+        private float _aimTimer = 0;
         private bool _keepAiming;
         private GameObject _player;
+        private bool _recticleIsOnWayUp = true;
+
+        //Visuals
+        private SpriteRenderer _recticleSprite;
+        private bool _shotIsOnCooldown;
 
         #endregion
 
@@ -31,54 +30,55 @@ namespace Characters
         {
             _aimRecticle = transform.GetChild(0).gameObject;
             _player = transform.parent.gameObject;
-        }
-
-        private IEnumerator AimTweening()
-        {
-            if (!_isAiming)
-            {
-                _isAiming = true;
-                if (_player.transform.localScale.x > 0)
-                {
-                    transform.DORotateQuaternion(Quaternion.Euler(0, 0, 100), _aimSpeed).SetEase(_aimEase);
-                    yield return new WaitForSeconds(_aimSpeed);
-                    transform.DORotateQuaternion(Quaternion.Euler(0, 0, 0), _aimSpeed).SetEase(_aimEase);
-                    yield return new WaitForSeconds(_aimSpeed);
-                }
-                else
-                {
-                    transform.DORotateQuaternion(Quaternion.Euler(0, 0, -100), _aimSpeed).SetEase(_aimEase);
-                    yield return new WaitForSeconds(_aimSpeed);
-                    transform.DORotateQuaternion(Quaternion.Euler(0, 0, 0), _aimSpeed).SetEase(_aimEase);
-                    yield return new WaitForSeconds(_aimSpeed);
-                }
-                _isAiming = false;
-            }
+            _recticleSprite = _aimRecticle.GetComponent<SpriteRenderer>();
         }
 
         private void Update()
         {
+            //_recticleSprite.enabled = _keepAiming;
 
-            if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.F) || Input.GetKeyUp(KeyCode.LeftAlt))
             {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
                 _keepAiming = true;
             }
 
-            if (Input.GetKeyDown(KeyCode.C))
+            if (Input.GetKeyDown(KeyCode.S))
             {
                 _keepAiming = false;
             }
 
-            if(Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.F))
+            if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.F) || Input.GetKeyUp(KeyCode.LeftAlt))
             {
                 Shoot(GetShotDirection());
+                transform.eulerAngles = new(0, 0, 0);
+                _recticleIsOnWayUp = true;
                 _keepAiming = false;
+                _aimTimer = 0;
             }
 
-            if (_keepAiming)
+            if (_keepAiming && !_shotIsOnCooldown)
             {
-                StartCoroutine(AimTweening());
+                float maxAngle = _player.transform.localScale.x > 0 ? 100 : -100;
+                float angle = _recticleIsOnWayUp ? Mathf.Lerp(0, maxAngle, _aimTimer / _aimSpeed) : Mathf.Lerp(maxAngle, 0, _aimTimer /_aimSpeed);
+                transform.eulerAngles = new(0, 0, angle);
+                _aimTimer += Time.deltaTime;
+
+                if (Mathf.Abs(angle) >= Mathf.Abs(maxAngle) && _recticleIsOnWayUp)
+                {
+                    _recticleIsOnWayUp = false;
+                    transform.eulerAngles = _player.transform.localScale.x > 0 ? new(0, 0, 100) : new(0, 0, -100);
+                    _aimTimer = 0;
+                }
+
+                if (Mathf.Abs(angle) < 0.01f && !_recticleIsOnWayUp)
+                {
+                    _recticleIsOnWayUp = true;
+                    transform.eulerAngles = Vector3.zero;
+                    _aimTimer = 0;
+                }
             }
+
         }
 
         private Vector2 GetShotDirection()
@@ -92,9 +92,7 @@ namespace Characters
             Rigidbody2D rigidbody = shot.GetComponent<Rigidbody2D>();
             rigidbody.velocity = direction * _shotSpeed;
         }
-
-
-
+    
         #endregion
     }
 }
