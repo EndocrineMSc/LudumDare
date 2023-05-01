43ef946f9c90192530f047597e93f24ed855aa45
EndocrineMSc <bamsc.studio@gmail.com>
2023-05-01 16:23:22 +0200

    power up visuals

--- Assets/TimePowerUp.cs
+++ Assets/TimePowerUp.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Characters;
 using Utilites;
+using DG.Tweening;
 
 public class TimePowerUp : MonoBehaviour
 {
+    private void Start()
+    {
+        transform.DOScale(0.5f, 1f).SetLoops(-1, LoopType.Yoyo);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            transform.DOKill(gameObject);
             GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().DeliveryBagTimer += 10;
             LevelEvents.Instance.TimePowerUpPickedUp?.Invoke();
             Destroy(gameObject);
