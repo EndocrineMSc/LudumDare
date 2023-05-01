d6b09a5de68cc65e68cb4940d88f27bd4f4c51ec
EndocrineMSc <bamsc.studio@gmail.com>
2023-05-01 21:41:58 +0200

    Last push for real yo

--- Assets/Scripts/Utility/GameManager.cs
+++ Assets/Scripts/Utility/GameManager.cs
@@ -1,9 +1,11 @@
 using Audio;
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Utilities
 {
@@ -18,6 +20,8 @@ namespace Utilities
         public UnityEvent SettingsOpened;
         public UnityEvent CreditsOpened;
         public UnityEvent HowToPlayOpened;
+        private Image _introImage;
+        private Canvas _introCanvas;
 
         private readonly string LEVEL_ONE_PARAM = "LevelOne";
 
@@ -45,15 +49,19 @@ namespace Utilities
             SwitchState(GameState.MainMenu);
             Application.targetFrameRate = 60;
             SceneManager.sceneLoaded += OnSceneLoaded;
+            _introImage = GameObject.FindGameObjectWithTag("Intro").GetComponentInChildren<Image>();
+            _introCanvas = GameObject.FindGameObjectWithTag("Intro").GetComponent<Canvas>();
+            _introCanvas.enabled = false;
         }
 
-        internal void SwitchState (GameState state)
+        internal void SwitchState(GameState state)
         {
             State = state;
 
             switch (state)
             {
                 case GameState.MainMenu:
+                    Lives = 3;
                     AudioManager.Instance.FadeGameTrack(Track._001_Ambient_01, Fade.Out);
                     AudioManager.Instance.FadeGameTrack(Track._002_Ambient_02, Fade.Out);
                     MainMenuOpened?.Invoke();
@@ -68,9 +76,8 @@ namespace Utilities
                     HowToPlayOpened?.Invoke();
                     break;
                 case GameState.LevelOne:
-                    SceneManager.LoadSceneAsync(LEVEL_ONE_PARAM);
-                    AudioManager.Instance.FadeGameTrack(Track._001_Ambient_01, Fade.In);
-                    AudioManager.Instance.FadeGameTrack(Track._002_Ambient_02, Fade.In);
+                    _introCanvas.enabled = true;
+                    _introImage.DOFade(1, 1f);
                     break;
             }
         }
@@ -82,11 +89,28 @@ namespace Utilities
                 Debug.Log("Main Menu Loaded");
                 SwitchState(GameState.MainMenu);
                 GameObject.Find("MainMenuCanvas").GetComponent<Canvas>().enabled = true;
+                _introImage = GameObject.FindGameObjectWithTag("Intro").GetComponentInChildren<Image>();
+                _introCanvas = GameObject.FindGameObjectWithTag("Intro").GetComponent<Canvas>();
+                _introCanvas.enabled = false;
             }
         }
 
         #endregion
+
+
+        private void Update()
+        {
+            if (Input.anyKey && State == GameState.LevelOne)
+            {
+                SwitchState(GameState.MainMenu);
+                SceneManager.LoadSceneAsync(LEVEL_ONE_PARAM); 
+                AudioManager.Instance.FadeGameTrack(Track._001_Ambient_01, Fade.In);
+                AudioManager.Instance.FadeGameTrack(Track._002_Ambient_02, Fade.In);
+            }
+        }
     }
+
+
 
     internal enum GameState
     {
